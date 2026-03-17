0bd9fc6dcf196c25df14b23c0cc642d445595e9c
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2026-03-17 19:55:05 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="Expense Tracker ",page_icon="💰")
+
+st.title("💰 Advanced Expense tracker ")
+
+st.write(""" 
+         Welcome to your personal expense tracker .
+
+         use the sidebar widget:
+
+         1.Add Expense 
+         2.View Expenses
+         3.Dashboard
+         
+         
+         """)
+
+st.info("Track Your daily expenses easily ")
+
